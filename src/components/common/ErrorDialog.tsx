756761dee6c3b19926348e15864e2088c53116c7
Ko <ko.webcreator@gmail.com>
2023-02-28 16:28:35 +0900

    error dialog を作成しました。

--- src/components/common/ErrorDialog.tsx
+++ src/components/common/ErrorDialog.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import 'scss/pageComponents/common/errorDialog.scss'
-import { useAppSelector } from 'store'
-import { selectIsError } from 'store/common/errorSlice'
+import { ERROR_DIALOG_MSG } from 'consts/messages/error'
+import React, { useCallback } from 'react'
+import styles from 'scss/components/error.module.scss'
+import { useAppDispatch, useAppSelector } from 'store'
+import { changeIsErrorStatus, selectIsError } from 'store/common/errorSlice'
+import { CustomLink } from './CustomLink'
 
 export const ErrorDialog = () => {
   const isError = useAppSelector((state) => selectIsError(state))
+  const dispatch = useAppDispatch()
 
-  return isError ? <div>ErrorDialog</div> : null
+  const onClickCloseDialog = useCallback(() => {
+    dispatch(changeIsErrorStatus({ isError: false }))
+  }, [dispatch])
+
+  return isError ? (
+    <div className={styles.error}>
+      <article>
+        <h2>{ERROR_DIALOG_MSG}</h2>
+        <CustomLink onClick={onClickCloseDialog}>閉じる</CustomLink>
+      </article>
+    </div>
+  ) : null
 }
